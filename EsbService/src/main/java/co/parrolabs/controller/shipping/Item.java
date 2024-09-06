bd3642b3c11163def67caa291e83ef838ec1048b
gerardorave <gerardorave@gmail.com>
2024-09-06 14:50:40 -0500

    Changes for AWS Lambda client and Service

---
+++ EsbService/src/main/java/co/parrolabs/controller/shipping/Item.java
@@ -0,0 +1,36 @@
+package co.parrolabs.controller.shipping;
+
+import javax.persistence.*;
+@Embeddable
+public class Item {
+    private String location;
+    private String description;
+    private String timeStamp;
+
+    @Column(name = "location")
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Column(name = "description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Column(name = "timestamp")
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
--- EsbService/src/test/java/co/parrolabs/TestSecurityRoles.java
+++ EsbService/src/test/java/co/parrolabs/TestSecurityRoles.java
@@ -1,12 +1,16 @@
-package co.parrolabs.kafka.co.parrolabs;
+package co.parrolabs;
 
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.Assert.assertEquals;
 
+@ExtendWith(MockitoExtension.class)
 public class TestSecurityRoles {
-    @Test
+
     //@WithMockUser(username = "JOHN", authorities = { "SYS_ADMIN" })
+    @Test
     public void givenAuthoritySysAdmin_whenCallGetUsernameLC_thenReturnUsername() {
         //String username = userRoleService.getUsernameInLowerCase();
         String username ="john";
